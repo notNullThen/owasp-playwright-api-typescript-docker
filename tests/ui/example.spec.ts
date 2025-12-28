d3facd706863323bb715be31245c6089b35b8bcb
notNullThen <epekaryuk@outlook.com>
2025-12-28 11:39:58 +0100

    using 3 workers, verifying that users are diffrent for each worker

--- tests/ui/example.spec.ts
+++ tests/ui/example.spec.ts
@@ -1,7 +1,33 @@
 import { expect, test } from "../global-setup";
 
-test("has title", async ({ page }) => {
-  await page.goto("/");
+test.describe("Homepage", () => {
+  test("has title", async ({ page }) => {
+    await page.goto("/");
 
-  await expect(page.getByText("All Products")).toBeVisible();
+    await expect(page.getByText("All Products")).toBeVisible();
+
+    await page.locator("#navbarAccount").click();
+    const text = await page.getByRole("menuitem").first().innerText();
+    console.log("!#!#!# USER:", text);
+  });
+
+  test("has title 2", async ({ page }) => {
+    await page.goto("/");
+
+    await expect(page.getByText("All Products")).toBeVisible();
+
+    await page.locator("#navbarAccount").click();
+    const text = await page.getByRole("menuitem").first().innerText();
+    console.log("!#!#!# USER:", text);
+  });
+
+  test("has title 3", async ({ page }) => {
+    await page.goto("/");
+
+    await expect(page.getByText("All Products")).toBeVisible();
+
+    await page.locator("#navbarAccount").click();
+    const text = await page.getByRole("menuitem").first().innerText();
+    console.log("!#!#!# USER:", text);
+  });
 });
