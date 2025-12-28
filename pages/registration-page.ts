76e2b610cfe96707e3f6063a8c1425d095e19ccf
notNullThen <epekaryuk@outlook.com>
2025-12-28 17:52:28 +0100

    login test, registration page

--- pages/registration-page.ts
+++ pages/registration-page.ts
@@ -1,6 +1,7 @@
 import { Page } from "@playwright/test";
 import InputFormField from "../components/input-form-field";
 import PageBase from "./page-base";
+import Dropdown from "../components/dropdown";
 
 export default class RegistrationPage extends PageBase {
   constructor(page: Page) {
@@ -15,5 +16,23 @@ export default class RegistrationPage extends PageBase {
   }
   get repeatPasswordInput() {
     return new InputFormField(this.page).getByLocator("#repeatPasswordControl");
+  }
+  get securityQuestionDropdown() {
+    return new Dropdown(this.page).getByName("securityQuestion");
+  }
+  get answerInput() {
+    return new InputFormField(this.page).getByLocator("#securityAnswerControl");
+  }
+  get registerButton() {
+    return this.page.locator("button[type=submit]");
+  }
+
+  async submit() {
+    const [, userResponse] = await Promise.all([
+      this.registerButton.click(),
+      this.api.users.waitForPostUser(),
+      this.api.securityAnswers.waitForPostSecurityAnswers(),
+    ]);
+    return userResponse;
   }
 }
