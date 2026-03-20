66c674a1b83f687dbd4a021f62e7f33c875e7edb
notNullThen <epekaryuk@outlook.com>
2026-03-20 16:58:23 +0100

    iterable-component-base - small type adjustment

--- components/iterable-component-base.ts
+++ components/iterable-component-base.ts
@@ -3,20 +3,20 @@ import ComponentBase from "./component-base";
 export default abstract class IterableComponentBase extends ComponentBase {
   public count = async () => this.body.count();
 
-  public abstract getByText(text: string): ComponentBase;
-  public abstract getByIndex(index: number): ComponentBase;
+  public abstract getByText(text: string): IterableComponentBase;
+  public abstract getByIndex(index: number): IterableComponentBase;
 
-  protected getByTextBase<T extends ComponentBase>(text: string) {
+  protected getByTextBase<T extends IterableComponentBase>(text: string) {
     const component = this.create() as T;
     component.body = this.body.filter({ hasText: text });
     return component;
   }
 
-  protected getByIndexBase<T extends ComponentBase>(index: number) {
+  protected getByIndexBase<T extends IterableComponentBase>(index: number) {
     const component = this.create() as T;
     component.body = this.body.nth(index);
     return component;
   }
 
-  protected abstract create(): ComponentBase;
+  protected abstract create(): IterableComponentBase;
 }
